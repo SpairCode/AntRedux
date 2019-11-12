6d8633cea381245d2e15ee974ebd0a86eb31ee82
SpairCode <1598111569@qq.com>
2019-11-12 10:39:38 +0800

    2019-11-12 test state update

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ const store = createStore(counter)
 class App extends React.Component {
 
   state = {
-    number: 0
+    number: 0,
+    val: 0,
   }
 
   unsubscribe = () => {
@@ -24,6 +25,21 @@ class App extends React.Component {
 
   componentDidMount () {
     this.unsubscribe()
+
+    this.setState({val: this.state.val + 1});
+    console.log(this.state.val);    // 第 1 次输出  没有立即更新state
+
+    this.setState({val: this.state.val + 1});
+    console.log(this.state.val);    // 第 2 次输出  合并之前的state值  val: 1
+
+    setTimeout(() => {
+      this.setState({val: this.state.val + 1});
+      console.log(this.state.val);  // 第 3 次输出
+
+      this.setState({val: this.state.val + 1});
+      console.log(this.state.val);  // 第 4 次输出
+    }, 0);
+
   }
 
   // 提供 getState() 方法获取 state；
