d2867be462950fcc2d8e80e3b7994cbbf24f847b
SpairCode <1598111569@qq.com>
2019-12-10 17:23:38 +0800

    edit todolist

--- src/component/common/TodoList/TodoList.jsx
+++ src/component/common/TodoList/TodoList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Input, List, Button } from 'antd';
+import { Input, List, Button, Row, Col } from 'antd';
 import store from '../../../redux/store/store';
 
 class TodoList extends React.Component {
@@ -7,33 +7,61 @@ class TodoList extends React.Component {
   constructor (props) {
     super(props)
     this.state = store.getState()
+    store.subscribe(this.storeChange) //订阅Redux的状态
+  }
+
+  storeChange = () => {
+    this.setState(store.getState())
   }
 
   changeInputValue = (e) => {
-    console.log(e.target.value)
     const action = {
       type: 'changeInput',
       value: e.target.value
     }
     store.dispatch(action)
-    // store.subscribe()
+  }
+
+  addItem = () => {
+    const action = {
+      type: 'addItem',
+      value: this.state.inputValue
+    }
+    store.dispatch(action)
+  }
+
+  deleteItem = (index) => {
+    const action = {
+      type: 'deleteItem',
+      value: index
+    }
+    store.dispatch(action)
   }
 
   render () {
     const { inputValue, list }  = this.state
     return (
-      <div>
+      <div style={{ padding: 10 }}>
         <div>
           <Input onChange={ this.changeInputValue.bind() } style={{ width: 250, margin: '0px 10px 0px 0px' }} placeholder={ inputValue } />
-          <Button type="primary"> Add </Button>
+          <Button onClick={ this.addItem.bind() } type="primary"> Add </Button>
         </div>
         <div style={{ margin: '10px 0px' }}>
           <List
             header={<div> Working List Item </div>}
             bordered
             dataSource={list}
-            renderItem={item => (
-              <List.Item> {item} </List.Item>
+            renderItem={(item, index) => (
+              <List.Item>
+                <Row style={{ width: '100%' }}>
+                  <Col span={18} >
+                    { item }
+                  </Col>
+                  <Col span={6} >
+                    <Button onClick={ this.deleteItem.bind(this, index) } type="danger"> Delete </Button>
+                  </Col>
+                </Row>
+              </List.Item>
             )}
           />
         </div>
--- src/redux/reducers/todolist.js
+++ src/redux/reducers/todolist.js
@@ -15,8 +15,20 @@ export default (state = defaultState, action) => {
   // Reducer里面只能接收state, 不能改变state
   if (action.type === 'changeInput') {
     let newState = JSON.parse(JSON.stringify(state))
-    newState.inputValue = action.inputValue
-    return newState 
+    newState.inputValue = action.value
+    return newState
+  }
+  if (action.type === 'addItem') {
+    let newStates = JSON.parse(JSON.stringify(state)) 
+    newStates.list.push(newStates.inputValue)  //push新的内容到列表中去
+    newStates.inputValue = ''
+    return newStates
+  }
+  if (action.type === 'deleteItem') {
+    let newStates = JSON.parse(JSON.stringify(state)) 
+    newStates.list.splice(action.value, 1)  //push新的内容到列表中去
+    newStates.inputValue = action.inputValue
+    return newStates
   }
   return state
 }
