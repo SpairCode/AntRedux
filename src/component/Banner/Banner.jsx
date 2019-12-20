f41ef9ef4925733768a76b7a73d267aec5ef5569
SpairCode <1598111569@qq.com>
2019-12-20 17:58:18 +0800

    edit banner

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+// import { Row, Col } from 'antd';
 import styles from './Banner.less';
 
 class Banner extends React.Component {
   render () {
     return (
       <div className={styles.bannerBox}>
-        <Row>
-          <Col span={12}>
+        <div>
+          <div>
             <div className={`${styles.leftTextBox} animated fadeInLeft `}>
               <h1>How Does An Lcd Screen Work</h1>
               <span></span>
               <p>5 Reasons To Choose A Notebook Over A Computer <br/> Desktop</p>
             </div>
-          </Col>
-          <Col span={12}>
+          </div>
+          <div>
             {/* 图片 */}
             <div className={`${styles.imgBox} animated fadeInRight `}>
               <div className={styles.img}></div>
             </div>
-          </Col>
-        </Row>
+          </div>
+        </div>
       </div>
     )
   }
